9190b27f2af1abd18ebd0f789b8e368a264394da
Vincent Petithory <vincent.petithory@gmail.com>
2015-02-03 22:56:09 +0100

    add doc.go with instructions on how to use the dispel API

---
+++ doc.go
@@ -0,0 +1,46 @@
+// Package dispel implements code generation of server code for REST APIs written in Go.
+// The generated code is based on the JSON Hyper Schema describing the API.
+//
+// Its primary use is through go:generate, but it's also possible to invoke the dispel command directly,
+// or use this package in your code.
+//
+// Usage of this package usually begins with parsing a JSON Schema and the routes it contains:
+//
+//     var schema dispel.Schema
+//     err := json.NewDecoder(reader).Decode(&schema)
+//     // ...
+//     schemaParser := &dispel.SchemaParser{RootSchema: &schema}
+//     routes, err := schemaParser.ParseRoutes()
+//     // ...
+//
+// Then you want to generate code using these routes. Dispel provides several builtin templates
+// which provide code generation for registering API routes and API handlers, generating input and output types
+// for the data structures of these routes, and default implementations of API handlers.
+//
+// The TemplateBundle type is a bundle of all these templates; all templates for generating this code use the same template context:
+//
+//     tmpl, err := dispel.NewTemplateBundle(schemaParser)
+//     // ...
+//     ctx := &dispel.TemplateContext{
+//         Prgm:                "dispel",
+//         PkgName:             "main",
+//         Routes:              routes,
+//         HandlerReceiverType: "*App",
+//     }
+//     err := tmpl.ExecuteTemplate(os.Stdout, dispel.TemplateRoutes, ctx))
+//     // ...
+//
+// Dispel tries to be as unopiniated as possible for the generated code API. Most of the generated code makes use
+// of interfaces to let the developer implement them as he needs.
+// However, it's possible to generate default implementations of these interfaces to quickly have code that compiles.
+//
+// This is done with DefaultImplBundle type, which works similarly to the TemplateBundle.
+//
+//     tmpl, err := dispel.NewDefaultImplBundle()
+//     // ...
+//     pkgName := "main"
+//     err := tmpl.ExecuteTemplate(os.Stdout, dispel.DefaultImplMux, pkgName))
+//     // ...
+//
+// For a more detailed usage of this package, take a look at integration_test.go and cmd/dispel source.
+package dispel
