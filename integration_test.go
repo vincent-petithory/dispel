7c38c95d9d0de529ac2e852fa8a497d2dabf33ac
Vincent Petithory <vincent.petithory@gmail.com>
2015-02-09 18:00:36 +0100

    Refactor integration_test, add test with go generate

--- integration_test.go
+++ integration_test.go
@@ -9,6 +9,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -49,35 +50,92 @@ func equals(tb testing.TB, expected, actual interface{}) {
 	}
 }
 
-func TestGenerateAllFromJSONSchemaNoUserImplWithAPI(t *testing.T) {
-	if testing.Short() {
-		t.Skip("Skipping integration test")
+func makeGopathEnv(workspacedir string) []string {
+	var envs []string
+	for _, env := range os.Environ() {
+		if strings.HasPrefix(env, "GOPATH=") || strings.HasPrefix(env, "GOBIN=") {
+			continue
+		}
+		envs = append(envs, env)
 	}
-	generateAllFromJSONSchemaNoUserImpl(t, installWithDispelAPI)
+	envs = append(envs, "GOPATH="+workspacedir)
+	return envs
 }
 
-func TestGenerateAllFromJSONSchemaNoUserImplWithCmd(t *testing.T) {
-	if testing.Short() {
-		t.Skip("Skipping integration test")
-	}
-	generateAllFromJSONSchemaNoUserImpl(t, installWithDispelCmd)
+// IntegrationTest represents a test which generates a complete Go project
+// from a base of Go source files and a json schema, compiles it, runs the compiled program (usually a http server)
+// and tests the endpoints of this http server.
+type IntegrationTest struct {
+	InstallFn func(tb testing.TB, workspacedir string, pkgdir string)
+	TestFn    func(tb testing.TB, apiURL *url.URL)
 }
 
-func generateAllFromJSONSchemaNoUserImpl(t *testing.T, installFn func(tb testing.TB, destdir string)) {
+func (it *IntegrationTest) Run(tb testing.TB) {
 	tmpdir, err := ioutil.TempDir("", "dispel-")
 	if err != nil {
-		t.Fatal(err)
+		tb.Fatal(err)
 	}
 	defer func() {
-		if t.Failed() {
-			t.Logf("Failed in %q", tmpdir)
+		if tb.Failed() {
+			tb.Logf("Failed in %q", tmpdir)
 		} else {
 			os.RemoveAll(tmpdir)
 		}
 	}()
 
 	// Copy testdata tree
-	ok(t, filepath.Walk("testdata/rpg", func(path string, fi os.FileInfo, err error) error {
+	pkgdir, err := copyWorkspace(tmpdir)
+	ok(tb, err)
+
+	// Run install func
+	it.InstallFn(tb, tmpdir, pkgdir)
+
+	// Install generated project
+	_, err = exec.LookPath("go")
+	ok(tb, err)
+
+	installCmd := exec.Command("go", "install", "-v", "./...")
+	installCmd.Dir = pkgdir
+	installCmd.Env = makeGopathEnv(tmpdir)
+	out, err := installCmd.CombinedOutput()
+	if err != nil {
+		tb.Fatalf("%s\n\ngo install: %v", string(out), err)
+	}
+
+	// Control we have the binary
+	prgm := filepath.Join(tmpdir, "bin", filepath.Base(pkgdir))
+	_, err = os.Stat(prgm)
+	ok(tb, err)
+
+	// Run the server
+	var serverBuf bytes.Buffer
+	cmd := exec.Command(prgm, "--addr", "localhost:7777")
+	cmd.Stdout = &serverBuf
+	cmd.Stderr = &serverBuf
+	ok(tb, cmd.Start())
+	defer func() {
+		cmd.Process.Signal(syscall.SIGTERM)
+	}()
+	// Wait the http server comes up
+	time.Sleep(time.Millisecond * 500)
+
+	apiURL, err := url.Parse("http://localhost:7777")
+	ok(tb, err)
+	// Run tests
+	it.TestFn(tb, apiURL)
+
+	go func() {
+		cmd.Process.Signal(syscall.SIGTERM)
+	}()
+	if err := cmd.Wait(); err != nil {
+		tb.Error(serverBuf.String())
+	}
+}
+
+// copyWorkspace copies the test workspace and returns the abs path to the installed pkg.
+func copyWorkspace(destdir string) (string, error) {
+	workspaceRelpath := "testdata/test-workspace"
+	err := filepath.Walk(workspaceRelpath, func(path string, fi os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
@@ -85,76 +143,198 @@ func generateAllFromJSONSchemaNoUserImpl(t *testing.T, installFn func(tb testing
 			return filepath.SkipDir
 		}
 		if fi.IsDir() {
-			if path == "testdata/rpg" {
+			if path == workspaceRelpath {
 				return nil
 			}
-			destPath := strings.Replace(path, "testdata/rpg/", "", 1)
+			destPath := strings.Replace(path, workspaceRelpath+"/", "", 1)
 			if destPath == "" {
 				return nil
 			}
-			return os.MkdirAll(filepath.Join(tmpdir, destPath), 0777)
+			return os.MkdirAll(filepath.Join(destdir, destPath), 0777)
 		}
 
-		src, err := os.Open(path)
-		if err != nil {
+		destPath := filepath.Join(destdir, strings.Replace(path, workspaceRelpath+"/", "", 1))
+		if err := copyFile(destPath, path); err != nil {
 			return err
 		}
-		defer src.Close()
-
-		destPath := filepath.Join(tmpdir, strings.Replace(path, "testdata/rpg/", "", 1))
-		dest, err := os.Create(destPath)
-		if err != nil {
-			return err
-		}
-		defer dest.Close()
-
-		_, err = io.Copy(dest, src)
-		if err != nil {
-			return err
-		}
-
-		return err
-	}))
-	pkgdir := filepath.Join(tmpdir, "src", "rpg")
-
-	installFn(t, pkgdir)
-
-	// Install deps and compile generated project
-	_, err = exec.LookPath("go")
-	ok(t, err)
-
-	installCmd := exec.Command("go", "install", "-v", "./...")
-	installCmd.Dir = pkgdir
-	installCmd.Env = []string{}
-	for _, env := range os.Environ() {
-		if strings.HasPrefix(env, "GOPATH=") || strings.HasPrefix(env, "GOBIN=") {
-			continue
-		}
-		installCmd.Env = append(installCmd.Env, env)
-	}
-	installCmd.Env = append(installCmd.Env, "GOPATH="+tmpdir)
-	out, err := installCmd.CombinedOutput()
+		return nil
+	})
 	if err != nil {
-		t.Fatalf("%s\n\ngo install: %v", string(out), err)
+		return "", err
+	}
+	return filepath.Join(destdir, "src", "github.com", "vincent-petithory", "dispel", "apptest"), nil
+}
+
+func copyFile(destPath, srcPath string) error {
+	src, err := os.Open(srcPath)
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	dest, err := os.Create(destPath)
+	if err != nil {
+		return err
+	}
+	defer dest.Close()
+
+	_, err = io.Copy(dest, src)
+	if err != nil {
+		return err
 	}
 
-	// Control we have the binary
-	prgm := filepath.Join(tmpdir, "bin", filepath.Base(pkgdir))
-	_, err = os.Stat(prgm)
-	ok(t, err)
+	return nil
+}
 
-	// Run the server
-	var serverBuf bytes.Buffer
-	cmd := exec.Command(prgm, "--addr", "localhost:7777")
-	cmd.Stdout = &serverBuf
-	cmd.Stderr = &serverBuf
-	ok(t, cmd.Start())
-	defer func() {
-		cmd.Process.Signal(syscall.SIGTERM)
-	}()
-	// Wait the http server comes up
-	time.Sleep(time.Millisecond * 500)
+func TestGenerateAllFromRPGJSONSchemaNoUserImplWithAPI(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping integration test")
+	}
+	it := &IntegrationTest{
+		InstallFn: func(tb testing.TB, workspacedir string, pkgdir string) {
+			var schema dispel.Schema
+			f, err := os.Open("testdata/rpg.json")
+			ok(tb, err)
+			defer f.Close()
 
+			ok(tb, json.NewDecoder(f).Decode(&schema))
+
+			sp := &dispel.SchemaParser{RootSchema: &schema}
+			routes, err := sp.ParseRoutes()
+			ok(tb, err)
+
+			tmpl, err := dispel.NewTemplateBundle(sp)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			ctx := &dispel.TemplateContext{
+				Prgm:                "dispel",
+				PkgName:             "main",
+				Routes:              routes,
+				HandlerReceiverType: "*App",
+				ExistingHandlers:    []string{},
+			}
+
+			// Exec templates
+			var buf bytes.Buffer
+			for _, name := range tmpl.Names() {
+				ok(tb, tmpl.ExecuteTemplate(&buf, name, ctx))
+				// Format source with gofmt
+				src, err := format.Source(buf.Bytes())
+				if err != nil {
+					tb.Errorf("%s\n\ngofmt: %s", buf.Bytes(), err)
+					buf.Reset()
+					continue
+				}
+
+				// Write file to disk
+				ok(tb, ioutil.WriteFile(filepath.Join(pkgdir, fmt.Sprintf("dispel_%s.go", name)), src, 0666))
+				buf.Reset()
+			}
+
+			// Write defaults
+			defaultImpl, err := dispel.NewDefaultImplBundle()
+			ok(tb, err)
+			for _, name := range defaultImpl.Names() {
+				ok(tb, defaultImpl.ExecuteTemplate(&buf, name, ctx.PkgName))
+				// Format source with gofmt
+				src, err := format.Source(buf.Bytes())
+				if err != nil {
+					tb.Errorf("%s\n\ngofmt: %s", buf.Bytes(), err)
+					buf.Reset()
+					continue
+				}
+
+				// Write file to disk
+				ok(tb, ioutil.WriteFile(filepath.Join(pkgdir, name+".go"), src, 0666))
+				buf.Reset()
+			}
+		},
+		TestFn: testRPGSchemaAPINoImpl,
+	}
+	it.Run(t)
+}
+
+func TestGenerateAllFromRPGJSONSchemaNoUserImplWithCmd(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping integration test")
+	}
+	it := &IntegrationTest{
+		InstallFn: func(tb testing.TB, workspacedir string, pkgdir string) {
+			installDispelCmd := exec.Command("go", "install", "-v", "github.com/vincent-petithory/dispel/...")
+			out, err := installDispelCmd.CombinedOutput()
+			if err != nil {
+				tb.Fatalf("%s\n\ngo install: %v", string(out), err)
+			}
+
+			_, err = exec.LookPath("dispel")
+			ok(tb, err)
+
+			dispelCmd := exec.Command(
+				"dispel",
+				"-t", "all",
+				"-hrt", "*App",
+				"-d", "all",
+				"-pn", "main",
+				"-pp", pkgdir,
+				"testdata/rpg.json",
+			)
+			out, err = dispelCmd.CombinedOutput()
+			if err != nil {
+				tb.Fatalf("%s\n\ndispel: %v", string(out), err)
+			}
+		},
+		TestFn: testRPGSchemaAPINoImpl,
+	}
+	it.Run(t)
+}
+
+func TestGenerateAllFromRPGJSONSchemaNoUserImplWithGoGenerate(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping integration test")
+	}
+	it := &IntegrationTest{
+		InstallFn: func(tb testing.TB, workspacedir string, pkgdir string) {
+			installDispelCmd := exec.Command("go", "install", "-v", "github.com/vincent-petithory/dispel/...")
+			out, err := installDispelCmd.CombinedOutput()
+			if err != nil {
+				tb.Fatalf("%s\n\ngo install: %v", string(out), err)
+			}
+
+			_, err = exec.LookPath("dispel")
+			ok(tb, err)
+
+			pkgname, err := filepath.Rel(filepath.Join(workspacedir, "src"), pkgdir)
+			ok(tb, copyFile(filepath.Join(pkgdir, "schema.json"), "testdata/rpg.json"))
+
+			data := fmt.Sprintf("package main\n\n//go:generate %s\n", strings.Join([]string{
+				"dispel",
+				"-t", "all",
+				"-hrt", "*App",
+				"-d", "all",
+				"-pn", "main",
+				"-pp", pkgdir,
+				"schema.json",
+			}, " "))
+
+			ok(tb, ioutil.WriteFile(filepath.Join(pkgdir, "dispelgen.go"), []byte(data), 0666))
+			goGenerateCmd := exec.Command(
+				"go", "generate", "-x", pkgname,
+			)
+			goGenerateCmd.Env = makeGopathEnv(workspacedir)
+
+			out, err = goGenerateCmd.CombinedOutput()
+			if err != nil {
+				tb.Fatalf("%s\n\ngo:generate: %v", string(out), err)
+			}
+		},
+		TestFn: testRPGSchemaAPINoImpl,
+	}
+	it.Run(t)
+}
+
+func testRPGSchemaAPINoImpl(tb testing.TB, apiURL *url.URL) {
 	// Test endpoints return the expected status code, for the default implementations.
 	tests := []struct {
 		Method string
@@ -177,110 +357,18 @@ func generateAllFromJSONSchemaNoUserImpl(t *testing.T, installFn func(tb testing
 		{Method: "GET", Path: "/spells/fira", Code: 501},
 	}
 	for _, test := range tests {
-		urlStr := "http://localhost:7777" + test.Path
+		u := &(*apiURL)
+		u.Path = test.Path
 		var body io.Reader
 		if test.Body != nil {
 			body = bytes.NewReader(test.Body)
 		}
-		req, err := http.NewRequest(test.Method, urlStr, body)
+		req, err := http.NewRequest(test.Method, u.String(), body)
 		req.Header.Set("Content-Type", "application/json")
-		ok(t, err)
+		ok(tb, err)
 		resp, err := http.DefaultClient.Do(req)
-		ok(t, err)
+		ok(tb, err)
 		resp.Body.Close()
-		assert(t, resp.StatusCode == test.Code, "%s %q responded with %d", test.Method, test.Path, resp.StatusCode)
-	}
-
-	go func() {
-		cmd.Process.Signal(syscall.SIGTERM)
-	}()
-	if err := cmd.Wait(); err != nil {
-		t.Error(serverBuf.String())
-	}
-}
-
-func installWithDispelAPI(tb testing.TB, pkgdir string) {
-	var schema dispel.Schema
-	f, err := os.Open(filepath.Join(pkgdir, "schema.json"))
-	ok(tb, err)
-	defer f.Close()
-
-	ok(tb, json.NewDecoder(f).Decode(&schema))
-
-	sp := &dispel.SchemaParser{RootSchema: &schema}
-	routes, err := sp.ParseRoutes()
-	ok(tb, err)
-
-	tmpl, err := dispel.NewTemplateBundle(sp)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	ctx := &dispel.TemplateContext{
-		Prgm:                "dispel",
-		PkgName:             "main",
-		Routes:              routes,
-		HandlerReceiverType: "*App",
-		ExistingHandlers:    []string{},
-	}
-
-	// Exec templates
-	var buf bytes.Buffer
-	for _, name := range tmpl.Names() {
-		ok(tb, tmpl.ExecuteTemplate(&buf, name, ctx))
-		// Format source with gofmt
-		src, err := format.Source(buf.Bytes())
-		if err != nil {
-			tb.Errorf("%s\n\ngofmt: %s", buf.Bytes(), err)
-			buf.Reset()
-			continue
-		}
-
-		// Write file to disk
-		ok(tb, ioutil.WriteFile(filepath.Join(pkgdir, fmt.Sprintf("dispel_%s.go", name)), src, 0666))
-		buf.Reset()
-	}
-
-	// Write defaults
-	defaultImpl, err := dispel.NewDefaultImplBundle()
-	ok(tb, err)
-	for _, name := range defaultImpl.Names() {
-		ok(tb, defaultImpl.ExecuteTemplate(&buf, name, ctx.PkgName))
-		// Format source with gofmt
-		src, err := format.Source(buf.Bytes())
-		if err != nil {
-			tb.Errorf("%s\n\ngofmt: %s", buf.Bytes(), err)
-			buf.Reset()
-			continue
-		}
-
-		// Write file to disk
-		ok(tb, ioutil.WriteFile(filepath.Join(pkgdir, name+".go"), src, 0666))
-		buf.Reset()
-	}
-}
-
-func installWithDispelCmd(tb testing.TB, pkgdir string) {
-	installCmd := exec.Command("go", "install", "-v", "github.com/vincent-petithory/dispel/...")
-	out, err := installCmd.CombinedOutput()
-	if err != nil {
-		tb.Fatalf("%s\n\ngo install: %v", string(out), err)
-	}
-
-	_, err = exec.LookPath("dispel")
-	ok(tb, err)
-
-	dispelCmd := exec.Command(
-		"dispel",
-		"-t", "all",
-		"-hrt", "*App",
-		"-d", "all",
-		"-pn", "main",
-		"-pp", pkgdir,
-		filepath.Join(pkgdir, "schema.json"),
-	)
-	out, err = dispelCmd.CombinedOutput()
-	if err != nil {
-		tb.Fatalf("%s\n\ndispel: %v", string(out), err)
+		assert(tb, resp.StatusCode == test.Code, "%s %q responded with %d", test.Method, test.Path, resp.StatusCode)
 	}
 }
