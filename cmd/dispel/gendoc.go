69f5c3c5fca3ac63cbee0f1794c81879184bccf5
Vincent Petithory <vincent.petithory@gmail.com>
2015-02-12 01:45:01 +0100

    dispel cmd: generate cmd doc and flags help from a unique source
     * add gendoc and use it with go generate
     * generates a help.go file containing a helptext string var
       for use with flag package
     * generates a doc.go file containing the command documentation
     * move help source in help.txt

---
+++ cmd/dispel/gendoc.go
@@ -0,0 +1,100 @@
+// +build ignore
+
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"text/template"
+
+	"github.com/vincent-petithory/dispel"
+)
+
+var (
+	helpvar bool
+	godoc   bool
+)
+
+func init() {
+	flag.BoolVar(&helpvar, "helpvar", false, "generate help var")
+	flag.BoolVar(&godoc, "godoc", false, "generate cmd godoc")
+}
+
+func main() {
+	flag.Parse()
+
+	if !helpvar && !godoc {
+		flag.Usage()
+		log.Fatal("nothing to do")
+	}
+
+	if helpvar {
+		if err := generateHelpVar(); err != nil {
+			log.Println(err)
+		}
+	}
+
+	if godoc {
+		if err := generateGodoc(); err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func generateHelpVar() error {
+	f, err := os.Create("help.go")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	fmt.Fprintln(f, "package main")
+	fmt.Fprint(f, "\nvar helptext = `")
+	if err := executeHelpTemplate(f); err != nil {
+		return err
+	}
+	fmt.Fprintln(f, "`")
+	return nil
+}
+
+func generateGodoc() error {
+	var buf bytes.Buffer
+	if err := executeHelpTemplate(&buf); err != nil {
+		return err
+	}
+
+	f, err := os.Create("doc.go")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(&buf)
+	for scanner.Scan() {
+		fmt.Fprintf(f, "// %s\n", scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+	fmt.Fprintln(f, "package main")
+	return nil
+}
+
+func executeHelpTemplate(w io.Writer) error {
+	t, err := template.ParseFiles("help.txt")
+	if err != nil {
+		return err
+	}
+	return t.Execute(w, struct {
+		TemplateNames    []string
+		DefaultImplNames []string
+	}{
+		TemplateNames:    dispel.TemplateNames(),
+		DefaultImplNames: dispel.DefaultImplNames(),
+	})
+}
